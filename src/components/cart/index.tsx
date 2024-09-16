3442f44a5b8feb17a93cde2694c801074649e39e
Lucas Panizio <lucas_panizio@hotmail.com>
2024-09-16 19:41:54 -0300

    :lipstick: style: updated cart style

--- src/components/cart/index.tsx
+++ src/components/cart/index.tsx
@@ -8,12 +8,23 @@ export const Cart = () => {
   const { items, totalValue, totalQuantity, isOpen, removeItem, toggle } =
     useCart();
 
+  const hasAnyProduct = totalQuantity > 0;
+
   return (
     <>
       {isOpen && <div className="cart_overlay" onClick={toggle} />}
+
       <div className={`cart ${isOpen && 'cart_open'}`}>
         <div className="cart_title">
-          <span>Carrinho de compras ({totalQuantity} itens)</span>
+          <div>
+            <span>Carrinho de compras </span>
+            {hasAnyProduct && (
+              <span className="cart_title_items">
+                ({totalQuantity} {totalQuantity === 1 ? 'item' : 'itens'})
+              </span>
+            )}
+          </div>
+
           <button
             title="Fechar carrinho"
             className="cart_btn-close"
@@ -23,19 +34,32 @@ export const Cart = () => {
           </button>
         </div>
 
-        {items.map((product) => {
-          return (
-            <Product key={`${product.id}_${Math.random()}`} data={product}>
-              <Button
-                text="REMOVER"
-                bgColor="#FC2947"
-                handleClick={() => removeItem(product)}
-              />
-            </Product>
-          );
-        })}
+        <div className="cart_content">
+          {hasAnyProduct ? (
+            <>
+              {items.map((product) => {
+                return (
+                  <Product
+                    key={`${product.id}_${Math.random()}`}
+                    data={product}
+                  >
+                    <Button
+                      text="REMOVER"
+                      bgColor="#FC2947"
+                      handleClick={() => removeItem(product)}
+                    />
+                  </Product>
+                );
+              })}
+            </>
+          ) : (
+            <span className="noProduct">Nenhum produto adicionado.</span>
+          )}
+        </div>
 
-        <span>{formatCurrency(totalValue)}</span>
+        <div className="cart_footer">
+          <span>{formatCurrency(totalValue)}</span>
+        </div>
       </div>
     </>
   );
