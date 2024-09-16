8246dd95e827f4c8d4616080a56ce4d20179d7f8
Lucas Panizio <lucas_panizio@hotmail.com>
2024-09-16 15:51:45 -0300

    :recycle: refactor: updated icon, logo and app title

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,17 @@
 import { useCart } from '../../hooks/useCart';
+import logo from '../../assets/logo.png';
 import './styles.css';
 
 export const Header = () => {
   const { toggle } = useCart();
   return (
     <div className="header">
-      <div className="header_logo">Logo</div>
+      <div className="header_logo">
+        <img src={logo} alt="Logo" width="56px" />
+        <h1>Redux Store</h1>
+      </div>
       <button type="button" className="header_btn-cart" onClick={toggle}>
-        <i className="fa fa-shopping-cart"></i>
+        <i className="fa fa-shopping-cart" />
       </button>
     </div>
   );
