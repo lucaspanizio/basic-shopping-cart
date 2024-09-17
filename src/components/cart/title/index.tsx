9a48c64c59f7c811b11af12f6e22a05d62544fd4
Lucas Panizio <lucas_panizio@hotmail.com>
2024-09-17 02:17:14 -0300

    :recycle: refactor: shopping cart redesign

---
+++ src/components/cart/title/index.tsx
@@ -0,0 +1,27 @@
+import { useCart } from '../../../hooks/useCart';
+import './styles.css';
+
+export const Title = () => {
+  const { totalQuantity, hasAnyProduct, toggle } = useCart();
+
+  return (
+    <div className="cart-title">
+      <div>
+        <span>Carrinho de compras </span>
+        {hasAnyProduct && (
+          <span className="cart-title__items">
+            ({totalQuantity} {totalQuantity === 1 ? 'item' : 'itens'})
+          </span>
+        )}
+      </div>
+
+      <button
+        title="Fechar carrinho"
+        className="cart-title__btn-close"
+        onClick={toggle}
+      >
+        <i className="fa fa-close" />
+      </button>
+    </div>
+  );
+};
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,15 +1,13 @@
-import { List } from '../../components/list';
 import { Header } from '../../components/header';
+import { ProductList } from '../../components/productList';
 import { Cart } from '../../components/cart';
-import { useCart } from '../../hooks/useCart';
 
 export const Main = () => {
-  const { isOpen } = useCart();
   return (
     <>
       <Header />
-      <List />
-      {isOpen && <Cart />}
+      <ProductList />
+      <Cart />
     </>
   );
 };
