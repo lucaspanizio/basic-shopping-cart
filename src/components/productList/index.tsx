edcb35c1a8d7b5771080f8a8a3e7519a2e7551f9
Lucas Panizio <lucas_panizio@hotmail.com>
2024-09-18 17:00:01 -0300

    :recycle: refactor: upd productlist to get any product and render title

--- src/components/productList/index.tsx
+++ src/components/productList/index.tsx
@@ -1,57 +1,64 @@
 import { Navigation, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { formatCurrency } from '../../utils/formatCurrency';
-import { useFetchProducts } from '../../services/api/products';
 import { useCart } from '../../hooks/useCart';
+import { IProduct } from '../../store/cart/cart-types';
 import './styles.css';
 
-export const ProductList = () => {
+interface IProductListProps {
+  title: string;
+  data: IProduct[];
+}
+
+export const ProductList = ({ title, data: products }: IProductListProps) => {
   const { appendItem } = useCart();
-  const { data: products = [] } = useFetchProducts();
 
   return (
     <div className="product-list">
-      <Swiper
-        modules={[Navigation, Pagination]}
-        navigation={{ enabled: true }}
-        pagination={{ enabled: true, clickable: true }}
-        spaceBetween={10}
-        breakpoints={{
-          260: { slidesPerView: 2 },
-          640: { slidesPerView: 3 },
-          768: { slidesPerView: 4 },
-          1024: { slidesPerView: 5 },
-          1360: { slidesPerView: 7 },
-        }}
-      >
-        {products.map((product) => (
-          <SwiperSlide key={product.id}>
-            <div className="product-list__card">
-              <img
-                className="product-list__card-img"
-                src={product.image}
-                alt="Imagem do Produto"
-              />
-              <div className="product-list__card-overlay" />
-              <span className="product-list__card-description">
-                {product.title}
-              </span>
-              <span className="product-list__card-price">
-                {formatCurrency(product.price)}
-              </span>
-              <button
-                className="product-list__card-button"
-                onClick={() => appendItem(product)}
-              >
-                <span>
-                  COMPRAR&nbsp;
-                  <i className="fa fa-shopping-cart" />
+      <h1 className="product-list__title">{title}</h1>
+      <div className="product-list__slides">
+        <Swiper
+          modules={[Navigation, Pagination]}
+          navigation={{ enabled: true }}
+          pagination={{ enabled: true, clickable: true }}
+          spaceBetween={10}
+          breakpoints={{
+            260: { slidesPerView: 2 },
+            640: { slidesPerView: 3 },
+            768: { slidesPerView: 4 },
+            1024: { slidesPerView: 5 },
+            1360: { slidesPerView: 7 },
+          }}
+        >
+          {products.map((product) => (
+            <SwiperSlide key={product.id}>
+              <div className="product-list__slides-card">
+                <img
+                  className="product-list__slides-card-img"
+                  src={product.image}
+                  alt="Imagem do Produto"
+                />
+                <div className="product-list__slides-card-overlay" />
+                <span className="product-list__slides-card-description">
+                  {product.title}
                 </span>
-              </button>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+                <span className="product-list__slides-card-price">
+                  {formatCurrency(product.price)}
+                </span>
+                <button
+                  className="product-list__slides-card-button"
+                  onClick={() => appendItem(product)}
+                >
+                  <span>
+                    COMPRAR&nbsp;
+                    <i className="fa fa-shopping-cart" />
+                  </span>
+                </button>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 };
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,12 +1,17 @@
+import { useGetProducts } from '../../services/api/products';
 import { Header } from '../../components/header';
 import { ProductList } from '../../components/productList';
 import { Cart } from '../../components/cart';
 
 export const Main = () => {
+  const { data: smartphones = [] } = useGetProducts({ query: 'celular' });
+  const { data: notebooks = [] } = useGetProducts({ query: 'notebook' });
+
   return (
     <>
       <Header />
-      <ProductList />
+      <ProductList title="Notebooks" data={notebooks} />
+      <ProductList title="Celulares" data={smartphones} />
       <Cart />
     </>
   );
