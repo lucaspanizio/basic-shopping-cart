850c722d18a0b3734cf3f600b98ce5163cd9364b
Lucas Panizio <lucas_panizio@hotmail.com>
2024-09-18 17:00:01 -0300

    :recycle: refactor: upd ProductList to get any product and render title

--- src/services/api/products.ts
+++ src/services/api/products.ts
@@ -4,15 +4,16 @@ import { api } from '../api';
 
 const FIVE_MINUTES_MS = 1000 * 60 * 5;
 
-export const useFetchProducts = () =>
+export const useGetProducts = ({ query }: { query: string }) =>
   useQuery({
-    queryKey: ['products'],
+    queryKey: [`products_${query}`],
     queryFn: async () => {
       const { data } = await api.get<{ results: any[] }>('/search', {
         params: {
-          offset: 0,
-          limit: 50,
-          q: 'celular',
+          offset: 0, // A partir de qual registro começa a contar o limit
+          limit: 10, // Quantidade de registros
+          q: query, // Termo da busca
+          sort: 'sales_desc', // Ordena pelos mais vendidos
         },
       });
 
